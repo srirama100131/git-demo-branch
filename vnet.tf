af8360efdb4f6fb1e24ddc903fe36663ada57c48
rajubandi <rajubandi@gmail.com>
2026-03-29 17:42:54 +0530

    subnet added

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,10 @@ resource "azurerm_virtual_network" "vnets" {
     location            = azurerm_resource_group.name.location
     resource_group_name = azurerm_resource_group.name.name
 }
+
+resource "azurerm_subnet" "subnets" {
+  name                 = "subnet-terraform-demo"
+  resource_group_name  = azurerm_resource_group.name.name
+  virtual_network_name = azurerm_virtual_network.vnets.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
